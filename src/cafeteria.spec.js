0f3c68640b577166b3ea48847fa8b670d0fd405a
Elian <eliantito234@gmail.com>
2023-05-17 16:27:08 -0600

    Refactory en la prueba: deberia buscar el producto de la lista de reservas en la lista de productos

--- src/cafeteria.spec.js
+++ src/cafeteria.spec.js
@@ -18,7 +18,7 @@ describe("Reservar un producto de una lista Estatica", () => {
     expect(Reservar(["cafe","mocca"],["cafe","mocca"],[])).toEqual(["cafe","mocca"]);
   });
   it("deberia buscar el producto de la lista de reservas en la lista de productos ", () => {
-    expect(Reservar(["cafe","mocca"],["mocca"])).toEqual(["mocca"]);
+    expect(Reservar(["cafe","mocca"],["mocca"],[])).toEqual(["mocca"]);
   });
   it("deberia buscar los productos de la lista de reservas en la lista de productos y devolver la lista de reservas", () => {
     expect(Reservar(["cafe","mocca","te","sandwich"],["mocca","sandwich"])).toEqual(["mocca","sandwich"]);
