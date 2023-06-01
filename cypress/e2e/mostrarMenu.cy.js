86e804118772009cc8a25f4551de6f4998f29586
Elian <eliantito234@gmail.com>
2023-06-01 01:39:03 -0400

    Prueba en cypress de eliminar un producto en modo administrador, pasando

--- cypress/e2e/mostrarMenu.cy.js
+++ cypress/e2e/mostrarMenu.cy.js
@@ -102,10 +102,8 @@ describe("Prueba de vizualizacion de mis productos", () => {
     cy.get(".btn_editar").click();
     cy.get("#editarForm").should("be.visible");
   });
- 
-  
-    it('debería editar el formulario correctamente', () => {
-      cy.visit("/");
+  it("al hacer click en eliminar no deberia mostrar el producto en el menu", () => {
+    cy.visit("/");
     cy.get("#adminButton").click();
     cy.get("#nombre").type("Sopa");
     cy.get("#descripcion").type("Sopa de fideo");
@@ -117,16 +115,10 @@ describe("Prueba de vizualizacion de mis productos", () => {
     cy.get("#menu-div").should("contain", "Sopa de fideo");
     cy.get("#menu-div").should("contain", 20);
     cy.get("#menu-div").should("contain", 2);
-    cy.get('#menu-div').should('contain', 'almuerzos')
-    cy.get(".btn_editar").click();
-    cy.get("#editarForm").should("be.visible");
-    cy.get('#editNombre').clear().type('Nuevo Nombre');
-    cy.get('#editDescripcion').clear().type('Nueva Descripción');
-    cy.get('#editPrecio').clear().type('10');
-    cy.get('#editCantidad').clear().type('5');
-    cy.get('#editForm').submit();
-  
-
+    cy.get('#menu-div').should('contain', 'almuerzos');
+    cy.get(".btn_eliminar").click();
+    cy.get("#menu-div").should(($menuDiv) => {
+      expect("not.be.visible");
     });
   });
-  
+});
